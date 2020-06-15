b99d56f9462dd1f4fe425ee569053598baefa3be
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2020-06-15 14:48:23 -0300

    Adding fontmake and fontbakery automation
    for building TTFs and running quality-assurance checks frequently.
    (issue #7)

---
+++ build.sh
@@ -0,0 +1,24 @@
+rm output/ -rf
+
+for src in sources/*.glyphs
+do
+  fontmake -g $src -o ttf -i --output-dir output/
+done
+
+for font in output/*.ttf
+do
+  gftools fix-nonhinting $font $font
+  gftools fix-dsig $font --autofix
+done
+
+# Cleanup gftools mess:
+rm output/*-backup-fonttools-prep-gasp.ttf
+
+# cp METADATA.pb output
+# cp DESCRIPTION.*.html output
+
+export OPTIONS="--no-progress"
+export OPTIONS="$OPTIONS --exclude-checkid /check/ftxvalidator" # We lack this on Travis.
+export OPTIONS="$OPTIONS --exclude-checkid /check/varfont" # & this when making a variable font.
+export OPTIONS="$OPTIONS --loglevel INFO --ghmarkdown HankenGrotesk-fontbakery.md"
+fontbakery check-googlefonts $OPTIONS output/*.ttf
