894a1280180461ddadd5b422ee18b38609d87d3c
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2020-11-10 12:32:47 -0500

    build includes VFs

--- sources/build.sh
+++ sources/build.sh
@@ -3,11 +3,50 @@ set -e
 
 mkdir -p ../fonts ../fonts/ttf ../fonts/ttf/static
 
+
+
+# Generating VFS ==============================================================================================
+
+function postprocess_vf {
+    gftools fix-nonhinting $1 $1.fix
+    mv $1.fix $1
+    # gftools fix-vf-meta $1 $1.fix
+    # mv $1.fix $1
+    gftools fix-dsig -f $1
+    gftools fix-unwanted-tables $1 -t MVAR
+}
+
+echo "GENERATING VFs"
+
+VF_FILE=../fonts/ttf/HankenGrotesk\[wght]\.ttf
+fontmake -g HankenGrotesk.glyphs -o variable --output-path $VF_FILE
+
+VF_FILEit=../fonts/ttf/HankenGrotesk-Italic\[wght]\.ttf
+fontmake -g HankenGroteskItalic.glyphs -o variable --output-path $VF_FILEit
+
+
+echo "POST PROCESSING VFs"
+
+postprocess_vf $VF_FILE
+postprocess_vf $VF_FILEit
+
+gftools fix-vf-meta $VF_FILE
+mv $VF_FILE.fix $VF_FILE
+gftools fix-vf-meta $VF_FILEit
+mv $VF_FILEit.fix $VF_FILEit
+
+rm ../fonts/ttf/*gasp.ttf
+
+
+# Generating TTFS ==============================================================================================
+
 echo "GENERATING TTFs"
+
 fontmake -g HankenGrotesk.glyphs -i -o ttf --output-dir ../fonts/ttf/static
 fontmake -g HankenGroteskItalic.glyphs -i -o ttf --output-dir ../fonts/ttf/static
 
 echo "POST PROCESSING TTFs"
+
 ttfs=$(ls ../fonts/ttf/static/*.ttf)
 for ttf in $ttfs
 do
@@ -23,3 +62,5 @@ do
 	#fonttools ttLib.woff2 compress $ttf
     
 done
+
+rm -rf master_ufo/ instance_ufo/
